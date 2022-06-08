c17530bc634595e6b426b0b81605c9051f9f56f6
VU LONG VU <longvuit18@gmail.com>
2022-06-08 09:14:47 +0700

    change contract's name and add changeNFT function

--- contracts/NFTManager.sol
+++ contracts/NFTManager.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract ManageNFT {
+contract NFTManager {
     // event
-    event DepositNFT(address indexed owner, uint256 tokenId);
+    event EquipNFT(address indexed owner, uint256 tokenId);
     event WithdrawNFT(address indexed owner, uint256 tokenId);
+    event ChangeNFT(
+        address indexed owner,
+        uint256 preTokenId,
+        uint256 nextTokenId
+    );
     // variables
     IERC721 private nft;
     // mapping
@@ -25,14 +30,14 @@ contract ManageNFT {
         return pools[user];
     }
 
-    function depositNFT(uint256 tokenId) external {
+    function equipNFT(uint256 tokenId) external {
         address user = msg.sender;
         require(tokenId != 0, "ManageNFT: tokenId is zero");
 
         nft.transferFrom(user, address(this), tokenId);
 
         pools[user].push(tokenId);
-        emit DepositNFT(user, tokenId);
+        emit EquipNFT(user, tokenId);
     }
 
     function withdrawNFT(uint256 tokenId) external {
@@ -44,6 +49,23 @@ contract ManageNFT {
 
         delete pools[user][index];
         emit WithdrawNFT(user, tokenId);
+    }
+
+    function changeNFT(uint256 preTokenId, uint256 nextTokenId) external {
+        address user = msg.sender;
+        require(preTokenId != 0, "ManageNFT: tokenId is zero");
+        (bool hasExist, uint256 index) = _hasAsset(user, preTokenId);
+        require(hasExist, "ManageNFT: tokenId is not in pool");
+
+        // transfer pre nft to user
+        nft.transferFrom(address(this), user, preTokenId);
+
+        // user transfers nft to pool
+        nft.transferFrom(user, address(this), nextTokenId);
+        // update pool
+        delete pools[user][index];
+        pools[user].push(nextTokenId);
+        emit ChangeNFT(user, preTokenId, nextTokenId);
     }
 
     function _hasAsset(address user, uint256 tokenId)
---
+++ test/NFTManager.test.ts
@@ -0,0 +1,107 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("NFTManager contract", function () {
+  let nft: Contract;
+  let nftManager: Contract;
+  beforeEach(async () => {
+    const NFT = await ethers.getContractFactory("NFT");
+    nft = await NFT.deploy();
+
+    const NFTManager = await ethers.getContractFactory("NFTManager");
+    nftManager = await NFTManager.deploy(nft.address);
+  })
+  it("equipNFT success", async function () {
+    const [player] = await ethers.getSigners();
+    await nft.connect(player).mint(1);
+    await nft.connect(player).mint(2);
+    await nft.connect(player).mint(3);
+
+    await nft.connect(player).setApprovalForAll(nftManager.address, true);
+    await nftManager.connect(player).equipNFT(1);
+    await nftManager.connect(player).equipNFT(2);
+    await nftManager.connect(player).equipNFT(3);
+
+    const pools = await nftManager.getAllNFT(player.address);
+    expect(pools[0].toNumber()).to.equal(1);
+    expect(pools[1].toNumber()).to.equal(2);
+    expect(pools[2].toNumber()).to.equal(3);
+  });
+
+  it("withdraw success", async function () {
+    const [player] = await ethers.getSigners();
+    await nft.connect(player).mint(1);
+    await nft.connect(player).mint(2);
+    await nft.connect(player).mint(3);
+
+    await nft.connect(player).setApprovalForAll(nftManager.address, true);
+    await nftManager.connect(player).equipNFT(1);
+    await nftManager.connect(player).equipNFT(2);
+    await nftManager.connect(player).equipNFT(3);
+
+    await nftManager.connect(player).withdrawNFT(1);
+    await nftManager.connect(player).withdrawNFT(2);
+
+    const poolsAfterWithdraw = await nftManager.getAllNFT(player.address);
+
+    // update pool success
+    expect(poolsAfterWithdraw[0].toNumber()).to.equal(0);
+    expect(poolsAfterWithdraw[1].toNumber()).to.equal(0);
+    expect(poolsAfterWithdraw[2].toNumber()).to.equal(3);
+
+    // nft is withdrawn to the wallet of player
+
+    const ownerNFT1 = await nft.ownerOf(1);
+    const ownerNFT2 = await nft.ownerOf(2);
+
+    expect(ownerNFT1).to.equal(player.address);
+    expect(ownerNFT2).to.equal(player.address);
+
+  });
+
+  it("changes NFT success", async function () {
+    const [player] = await ethers.getSigners();
+    await nft.connect(player).mint(1);
+    await nft.connect(player).mint(2);
+    await nft.connect(player).mint(3);
+
+    await nft.connect(player).setApprovalForAll(nftManager.address, true);
+    await nftManager.connect(player).equipNFT(1);
+    await nftManager.connect(player).equipNFT(2);
+
+    await nftManager.connect(player).changeNFT(1, 3);
+
+    const poolsAfterChange = await nftManager.getAllNFT(player.address);
+
+    // update pool success
+    expect(poolsAfterChange[0].toNumber()).to.equal(0);
+    expect(poolsAfterChange[1].toNumber()).to.equal(2);
+    expect(poolsAfterChange[2].toNumber()).to.equal(3);
+
+    // nft is withdrawn to the wallet of player
+
+    const ownerNFT1 = await nft.ownerOf(1);
+
+    expect(ownerNFT1).to.equal(player.address);
+
+  });
+
+  it("Revert when nft not in pool", async () => {
+    const [player] = await ethers.getSigners();
+    await nft.connect(player).mint(1);
+    await nft.connect(player).mint(2);
+
+    await nft.connect(player).setApprovalForAll(nftManager.address, true);
+    await nftManager.connect(player).equipNFT(1);
+    await nftManager.connect(player).equipNFT(2);
+
+    await expect(nftManager.connect(player).withdrawNFT(5)).to.be.reverted;
+
+  })
+  it("Revert when tokenId's nft is zero", async () => {
+    const [player] = await ethers.getSigners();
+    await expect(nftManager.connect(player).equipNFT(0)).to.be.reverted;
+
+  })
+});
